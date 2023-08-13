55300ab815ac4d8925b5b3ac0947485c07d5f3a6
Facundo <facundogsb@gmail.com>
2023-08-13 13:21:14 -0300

    subiendo la primera version de dashboard

--- src/03-Home/05-Calendar/Calendar.jsx
+++ src/03-Home/05-Calendar/Calendar.jsx
@@ -1,16 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Calendar from 'react-calendar';
 import './index.css';
 
 const Calendars = () => {
+  const [selectedDate, setSelectedDate] = useState(new Date());
+
+  const handleDateChange = date => {
+    setSelectedDate(date);
+  };
   return (
     <div className='container-calendars'>
       <h1>Calendario</h1>
       <div className='flex-calendars'>
         <div className='calendar'>
-          <h3>calendario</h3>
+          <Calendar
+            onChange={handleDateChange}
+            defaultActiveStartDate={selectedDate}
+          />
         </div>
         <div className='calendar'>
-        <h3>calendario</h3>
+          <Calendar
+            onChange={handleDateChange}
+            defaultActiveStartDate={selectedDate}
+          />
         </div>
       </div>
     </div>
